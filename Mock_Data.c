33b5a373e4e279ee714379b9c6310d8584b59bd4
Arya Taghizadeh <100978388+ariatgz@users.noreply.github.com>
2024-10-02 09:47:53 -0400

    Add files via upload

---
+++ Mock_Data.c
@@ -0,0 +1,23 @@
+
+/*
+
+CAN_FRAME_1= 
+
+
+    Handle = hcan3;
+    ID = CAN_ID_EXT;
+    Data Length = sizeof(data);
+    time_stamp= NOW;
+    Data = An Array of 1 Byte or 8 bits
+
+
+Motor_Controller_K1:
+
+    Driving_Directional_k
+
+
+
+
+
+
+*/
